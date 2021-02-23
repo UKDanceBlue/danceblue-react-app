1fac1a046897e4fb05f8356f08016009590d9920
Kenton Carrier <kncarrier28@gmail.com>
2021-02-22 20:06:53 -0500

    fix(eslint): Move properties out of class declaration
    JS Standard updated to only allow functions instead of properties, so I 
    moved the static navigationOptions outside of the class declaration.
    
    https://stackoverflow.com/questions/53256854/standardjs-linting-returns-parsing-error-for-static-variable

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,34 +1,27 @@
-﻿// Import third-party dependencies
-import React from "react";
+// Import third-party dependencies
+import React from 'react'
 import {
   SafeAreaView,
   StyleSheet,
-  View,
-  ScrollView,
-  Text,
-  StatusBar,
-} from "react-native";
-import { Header } from "react-native-elements";
+  ScrollView
+} from 'react-native'
 
-import Sponsors from "../../components/sponsor/Sponsors";
-import HeaderImage from "../../components/countdown/HeaderImage";
-import CountdownView from "../../components/countdown/CountdownView";
-import Announcements from "../../components/announcement/Announcements";
-import Standings from "../../components/Standings/Standings";
+import Sponsors from '../../components/sponsor/Sponsors'
+import HeaderImage from '../../components/countdown/HeaderImage'
+import CountdownView from '../../components/countdown/CountdownView'
+import Announcements from '../../components/announcement/Announcements'
+import Standings from '../../components/Standings/Standings'
 
 // Component for home screen in main navigation
 export class HomeScreen extends React.Component {
-  const navigationOptions = {
-    title: "Home",
-  };
-
-  render() {
-    const { navigate } = this.props.navigation;
-    const countDownEnd = new Date("2021-04-10");
+  render () {
+    /* eslint-disable */
+    const { navigate } = this.props.navigation
+    const countDownEnd = new Date('2021-04-10')
     const secondsToEnd = Math.floor(
       (countDownEnd.getTime() - Date.now()) / 1000
-    );
-    const eventName = "!! DanceBlue 2021 !!";
+    )
+    const eventName = '!! DanceBlue 2021 !!'
 
     return (
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -40,16 +33,20 @@ export class HomeScreen extends React.Component {
           <Sponsors />
         </SafeAreaView>
       </ScrollView>
-    );
+    )
   }
+}
+
+HomeScreen.navigationOptions = {
+  title: 'Home'
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: "100%",
-    width: "100%",
+    height: '100%',
+    width: '100%',
     marginTop: 5,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
--- src/screens/More/index.js
+++ src/screens/More/index.js
@@ -1,128 +1,129 @@
 // Import third-party dependencies
-import React from "react";
+import React from 'react'
 import {
   View,
   ImageBackground,
   TouchableHighlight,
   Text,
   SafeAreaView
-} from "react-native";
-import * as WebBrowser from 'expo-web-browser';
-import { createStackNavigator } from "@react-navigation/stack";
+} from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-import ProfileScreen from "./Profile";
-import { FAQScreen } from "./FAQ";
-import { DonateScreen } from "./Donate";
-import { ContactScreen } from "./Contact";
+import ProfileScreen from './Profile'
+import { FAQScreen } from './FAQ'
+import { DonateScreen } from './Donate'
+import { ContactScreen } from './Contact'
 
-import { styles } from "../../styles";
+import { styles } from '../../styles'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
-const profileButtonImage = require("./Profile_Button.jpg");
-const donateButtonImage = require("./Donate_Button.jpg");
-const faqsButtonImage = require("./FAQs_Button.jpg");
-const contactButtonImage = require("./Contact_Button.jpg");
+const profileButtonImage = require('./Profile_Button.jpg')
+const donateButtonImage = require('./Donate_Button.jpg')
+const faqsButtonImage = require('./FAQs_Button.jpg')
+const contactButtonImage = require('./Contact_Button.jpg')
 
 class MoreScreenOptions extends React.Component {
-  render() {
-    const { navigate } = this.props.navigation;
+  render () {
+    const { navigate } = this.props.navigation
     return (
       <>
-      <SafeAreaView style={{flex: 1}}>
-        <TouchableHighlight
-          onPress={() => {
-            navigate("Profile");
-          }}
-          style={styles.button}
-        >
-          <ImageBackground
-            source={profileButtonImage}
-            style={styles.img}
-            imageStyle={{ borderRadius: 10 }}
+        <SafeAreaView style={{ flex: 1 }}>
+          <TouchableHighlight
+            onPress={() => {
+              navigate('Profile')
+            }}
+            style={styles.button}
           >
-            <View style={styles.view}>
-              <Text style={styles.text}>
-                Profile
-              </Text>
-            </View>
-          </ImageBackground>
-        </TouchableHighlight>
-        <TouchableHighlight
-        onPress={() => {
-          navigate("Donate");
-        }}
-          style={styles.button}
-        >
-          <ImageBackground
-            source={donateButtonImage}
-            style={styles.img}
-            imageStyle={{ borderRadius: 10 }}
+            <ImageBackground
+              source={profileButtonImage}
+              style={styles.img}
+              imageStyle={{ borderRadius: 10 }}
+            >
+              <View style={styles.view}>
+                <Text style={styles.text}>
+                  Profile
+                </Text>
+              </View>
+            </ImageBackground>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              navigate('Donate')
+            }}
+            style={styles.button}
           >
-            <View style={styles.view}>
-              <Text style={styles.text}>
-                Donate
-              </Text>
-            </View>
-          </ImageBackground>
-        </TouchableHighlight>
-        <TouchableHighlight
-        onPress={() => {
-          navigate("FAQ");
-        }}
-          style={styles.button}
-        >
-          <ImageBackground
-            source={faqsButtonImage}
-            style={styles.img}
-            imageStyle={{ borderRadius: 10 }}
+            <ImageBackground
+              source={donateButtonImage}
+              style={styles.img}
+              imageStyle={{ borderRadius: 10 }}
+            >
+              <View style={styles.view}>
+                <Text style={styles.text}>
+                  Donate
+                </Text>
+              </View>
+            </ImageBackground>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              navigate('FAQ')
+            }}
+            style={styles.button}
           >
-            <View style={styles.view}>
-              <Text style={styles.text}>
-                FAQs
-              </Text>
-            </View>
-          </ImageBackground>
-        </TouchableHighlight>
-        <TouchableHighlight
-        onPress={() => {
-          navigate("Contact");
-        }}
-          style={styles.button}
-        >
-          <ImageBackground
-            source={contactButtonImage}
-            style={styles.img}
-            imageStyle={{ borderRadius: 10 }}
+            <ImageBackground
+              source={faqsButtonImage}
+              style={styles.img}
+              imageStyle={{ borderRadius: 10 }}
+            >
+              <View style={styles.view}>
+                <Text style={styles.text}>
+                  FAQs
+                </Text>
+              </View>
+            </ImageBackground>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              navigate('Contact')
+            }}
+            style={styles.button}
           >
-            <View style={styles.view}>
-              <Text style={styles.text}>
-                Contact
-              </Text>
-            </View>
-          </ImageBackground>
-        </TouchableHighlight>
+            <ImageBackground
+              source={contactButtonImage}
+              style={styles.img}
+              imageStyle={{ borderRadius: 10 }}
+            >
+              <View style={styles.view}>
+                <Text style={styles.text}>
+                  Contact
+                </Text>
+              </View>
+            </ImageBackground>
+          </TouchableHighlight>
         </SafeAreaView>
       </>
-    );
+    )
   }
 }
 
 // Component for "more" screen in main navigation
 export class MoreScreen extends React.Component {
-  const navigationOptions = {
-    title: "More"
-  };
-  render() {
-    const { navigate } = this.props.navigation;
+  render () {
+    /* eslint-disable */
+    const { navigate } = this.props.navigation
     return (
       <Stack.Navigator>
-        <Stack.Screen name="More Options" component={MoreScreenOptions} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="FAQ" component={FAQScreen} />
-        <Stack.Screen name="Donate" component={DonateScreen} />
-        <Stack.Screen name="Contact" component={ContactScreen} />
+        <Stack.Screen name='More Options' component={MoreScreenOptions} />
+        <Stack.Screen name='Profile' component={ProfileScreen} />
+        <Stack.Screen name='FAQ' component={FAQScreen} />
+        <Stack.Screen name='Donate' component={DonateScreen} />
+        <Stack.Screen name='Contact' component={ContactScreen} />
       </Stack.Navigator>
-    );
+    )
   }
+}
+
+MoreScreen.navigationOptions = {
+  title: 'More'
 }
