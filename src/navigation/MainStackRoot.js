5144b9e15692b53ccac37db7ea9a2491e8ba6d4f
Tag Howard <tag.howard1080@gmail.com>
2021-11-10 15:59:25 -0500

    fix(EventScreen) Click an event  to open EventView
    The route for navigating to an event was opining EventScreen instead
    of EventView

--- src/navigation/MainStackRoot.js
+++ src/navigation/MainStackRoot.js
@@ -6,7 +6,7 @@ import { BlurView } from 'expo-blur';
 
 // Import first-party dependencies
 import ScoreboardScreen from '../screens/ScoreBoardScreen';
-import EventScreen from '../screens/EventScreen';
+import { EventView } from '../screens/EventScreen';
 import GenericWebviewScreen from '../screens/GenericWebviewScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import TabBar from './TabBar';
@@ -14,40 +14,40 @@ import TabBar from './TabBar';
 const MainStack = createStackNavigator();
 
 const MainStackRoot = () => (
-    <MainStack.Navigator>
-      <MainStack.Screen name="Tab" component={TabBar} options={{ headerShown: false }} />
-      <MainStack.Screen name="Profile" component={ProfileScreen} />
-      <MainStack.Screen
-        name="FAQ"
-        component={GenericWebviewScreen}
-        initialParams={{
-          uri: 'https://www.danceblue.org/frequently-asked-questions/',
-        }}
-      />
-      <MainStack.Screen
-        name="Donate"
-        component={GenericWebviewScreen}
-        initialParams={{ uri: 'https://danceblue.networkforgood.com' }}
-      />
-      <MainStack.Screen
-        name="About"
-        component={GenericWebviewScreen}
-        initialParams={{ uri: 'https://www.danceblue.org/about/' }}
-      />
-      <MainStack.Screen name="Scoreboard" component={ScoreboardScreen} />
-      <MainStack.Screen
-        name="Event"
-        component={EventScreen}
-        options={({ route }) => ({
-          title: route.params.name,
-          headerTransparent: true,
-          headerMode: 'screen',
-          headerBackground: () => (
-            <BlurView tint="light" intensity={100} style={StyleSheet.absoluteFill} />
-          ),
-        })}
-      />
-    </MainStack.Navigator>
-  );
+  <MainStack.Navigator>
+    <MainStack.Screen name="Tab" component={TabBar} options={{ headerShown: false }} />
+    <MainStack.Screen name="Profile" component={ProfileScreen} />
+    <MainStack.Screen
+      name="FAQ"
+      component={GenericWebviewScreen}
+      initialParams={{
+        uri: 'https://www.danceblue.org/frequently-asked-questions/',
+      }}
+    />
+    <MainStack.Screen
+      name="Donate"
+      component={GenericWebviewScreen}
+      initialParams={{ uri: 'https://danceblue.networkforgood.com' }}
+    />
+    <MainStack.Screen
+      name="About"
+      component={GenericWebviewScreen}
+      initialParams={{ uri: 'https://www.danceblue.org/about/' }}
+    />
+    <MainStack.Screen name="Scoreboard" component={ScoreboardScreen} />
+    <MainStack.Screen
+      name="Event"
+      component={EventView}
+      options={({ route }) => ({
+        title: route.params.name,
+        headerTransparent: true,
+        headerMode: 'screen',
+        headerBackground: () => (
+          <BlurView tint="light" intensity={100} style={StyleSheet.absoluteFill} />
+        ),
+      })}
+    />
+  </MainStack.Navigator>
+);
 
 export default MainStackRoot;
