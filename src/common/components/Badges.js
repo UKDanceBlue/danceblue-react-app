280512ab3d4c087e9ef5eb2f2e36400b3909179c
Tag Howard <tag.howard1080@gmail.com>
2021-11-09 21:58:33 -0500

    refactor(Badges) Move to functional component

--- src/common/components/Badges.js
+++ src/common/components/Badges.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet, ActivityIndicator } from 'react-native';
 import { FlatGrid } from 'react-native-super-grid';
 
@@ -9,64 +9,45 @@ import { withFirebaseHOC } from '../../firebase/FirebaseContext';
 /**
  * A row of a user's {@link Badge}s loaded from Firebase
  * @param {Object} props Properties of the component: (imageURL, name)
- * @class
  */
-class Badges extends React.Component {
-  constructor(props) {
-    super(props);
+const Badges = ({ userID, firestore }) => {
+  //userID used to also be part of state, I couldn't find where that was used so I removed it, if this stops working; check there
+  [badges, setBadges] = useState(true);
+  [isLoading, setIsLoading] = useState(true);
 
-    this.state = {
-      userID: props.userID,
-      badges: [],
-      isLoading: true,
-    };
-  }
-
-  /**
-   * Called immediately after a component is mounted. Setting state here will trigger re-rendering.
-   */
-  componentDidMount() {
-    const badges = [];
-    this.props.firestore.getUserBadges(this.state.userID).then((snapshot) => {
+  useEffect(() => {
+    const loadedBadges = [];
+    firestore.getUserBadges(userID).then((snapshot) => {
       snapshot.forEach((doc) => {
-        badges.push(doc.data());
+        loadedBadges.push(doc.data());
       });
-      this.setState({ badges, isLoading: false });
+      setBadges(loadedBadges);
+      setIsLoading(false);
     });
-  }
+  });
 
-  /**
-   * Called to generate a React Native component
-   * @returns A JSX formatted component
-   */
-  render() {
-    return (
-      <>
-        {this.state.isLoading && (
-          <ActivityIndicator style={styles.image} size="large" color="blue" />
-        )}
-        {!this.state.isLoading && (
-          <>
-            {this.state.badges.length === 0 ? (
-              <Text>
-                You currently have no badges. Check back later to see if you've earned any!
-              </Text>
-            ) : (
-              <FlatGrid
-                itemDimension={130}
-                data={this.state.badges}
-                spacing={10}
-                renderItem={({ item }) => (
-                  <Badge name={item.name} imageURL={item.image} time={item.timeEarned} />
-                )}
-              />
-            )}
-          </>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {isLoading && <ActivityIndicator style={styles.image} size="large" color="blue" />}
+      {!isLoading && (
+        <>
+          {badges.length === 0 ? (
+            <Text>You currently have no badges. Check back later to see if you've earned any!</Text>
+          ) : (
+            <FlatGrid
+              itemDimension={130}
+              data={badges}
+              spacing={10}
+              renderItem={({ item }) => (
+                <Badge name={item.name} imageURL={item.image} time={item.timeEarned} />
+              )}
+            />
+          )}
+        </>
+      )}
+    </>
+  );
+};
 
 const styles = StyleSheet.create({});
 
