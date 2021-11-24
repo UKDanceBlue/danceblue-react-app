561e524bbc5fdff86a54d3ec29c2f47f011a651c
Tag Howard <tag.howard1080@gmail.com>
2021-11-24 16:30:16 -0500

    test(HeaderIcons)

---
+++ __test__/component/HeaderIcons.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { loadAsync } from 'expo-font';
+import { FontAwesome5 } from '@expo/vector-icons';
+import HeaderIcons from '../../src/navigation/HeaderIcons';
+
+beforeAll(async () => loadAsync(FontAwesome5.font));
+
+test('renders correctly', () => {
+  const tree = renderer.create(<HeaderIcons />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
