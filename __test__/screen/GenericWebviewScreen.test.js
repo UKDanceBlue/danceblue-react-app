382820114759f1298ade599c46dcf67c46aceae6
Tag Howard <tag.howard1080@gmail.com>
2021-11-24 16:30:32 -0500

    test(GenericWebviewScreen) 404 test

--- __test__/screen/GenericWebviewScreen.test.js
+++ __test__/screen/GenericWebviewScreen.test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import GenericWebviewScreen from '../../src/screens/GenericWebviewScreen';
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <GenericWebviewScreen route={{ params: { uri: 'www.example.com' } }} />
-  );
-  expect(tree).toMatchSnapshot();
+describe('generic webview', () => {
+  test('renders correctly', () => {
+    const tree = renderer
+      .create(<GenericWebviewScreen route={{ params: { uri: 'www.example.com' } }} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('shows 404 with no uri', () => {
+    const tree = renderer.create(<GenericWebviewScreen route={{ params: {} }} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
