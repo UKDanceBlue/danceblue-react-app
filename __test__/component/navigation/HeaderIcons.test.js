315234cdf619994f9c2249b7bff4aba9059099ae
Tag Howard <tag.howard1080@gmail.com>
2021-11-24 16:56:31 -0500

    refactor(HeaderIcons.test.js) move to own folder

--- __test__/component/navigation/HeaderIcons.test.js
+++ __test__/component/navigation/HeaderIcons.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { loadAsync } from 'expo-font';
 import { FontAwesome5 } from '@expo/vector-icons';
-import HeaderIcons from '../../src/navigation/HeaderIcons';
+import HeaderIcons from '../../../src/navigation/HeaderIcons';
 
 beforeAll(async () => loadAsync(FontAwesome5.font));
 
