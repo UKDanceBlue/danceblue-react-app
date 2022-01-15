2e8c1c1cffd91487a025260c58518c73bca1b38e
Tag Howard <tag.howard1080@gmail.com>
2022-01-15 12:48:54 -0500

    feat(Login Modal) Don't flash modal on app load

--- src/navigation/RootScreen.js
+++ src/navigation/RootScreen.js
@@ -2,37 +2,66 @@ import React, { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useSelector } from 'react-redux';
+import { ActivityIndicator } from 'react-native';
 import SplashLogin from '../screens/Modals/SplashLogin';
 import MainStackRoot from './MainStackRoot';
 import GenericWebviewScreen from '../screens/GenericWebviewScreen';
+import { globalColors } from '../theme';
 
 const RootStack = createStackNavigator();
 
-const RootScreen = ({ navigation }) => {
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+const RootScreen = () => {
   const isAuthLoaded = useSelector((state) => state.auth.isAuthLoaded);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const navigation = useNavigation();
 
   useEffect(() => {
-    if (isAuthLoaded && !isLoggedIn) {
-      navigation.navigate('SplashLogin');
+    if (isAuthLoaded && navigation.getState()) {
+      if (isLoggedIn) {
+        if (
+          navigation.getState().routes[navigation.getState().index].name === 'SplashLogin' &&
+          navigation.canGoBack()
+        ) {
+          navigation.goBack();
+        }
+      } else {
+        navigation.navigate('SplashLogin');
+      }
     }
   }, [navigation, isAuthLoaded, isLoggedIn]);
 
   return (
     <>
-      <RootStack.Navigator>
-        <RootStack.Screen name="Main" component={MainStackRoot} options={{ headerShown: false }} />
-        <RootStack.Screen
-          name="SplashLogin"
-          component={SplashLogin}
-          options={{ headerShown: false, presentation: 'modal' }}
+      {!isAuthLoaded && (
+        <ActivityIndicator
+          size="large"
+          color={globalColors.lightBlue}
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 20,
+          }}
         />
-        <RootStack.Screen
-          name="DefaultRoute"
-          component={GenericWebviewScreen}
-          options={{ headerBackTitle: 'Back', headerTitle: 'DanceBlue' }}
-        />
-      </RootStack.Navigator>
+      )}
+      {isAuthLoaded && (
+        <RootStack.Navigator>
+          <RootStack.Screen
+            name="Main"
+            component={MainStackRoot}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="SplashLogin"
+            component={SplashLogin}
+            options={{ headerShown: false, presentation: 'modal', gestureEnabled: false }}
+          />
+          <RootStack.Screen
+            name="DefaultRoute"
+            component={GenericWebviewScreen}
+            options={{ headerBackTitle: 'Back', headerTitle: 'DanceBlue' }}
+          />
+        </RootStack.Navigator>
+      )}
     </>
   );
 };
