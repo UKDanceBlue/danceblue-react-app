1a68774202ad4e48d9eaf431ed3f203c896b5bf6
Tag Howard <tag.howard1080@gmail.com>
2022-01-15 12:46:11 -0500

    refactor(nav) move Container to App.js

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 // Import third-party dependencies
 import React from 'react';
-import { StatusBar, LogBox } from 'react-native';
+import { StatusBar, LogBox, Linking } from 'react-native';
 import * as Notifications from 'expo-notifications';
 import { useAssets } from 'expo-asset';
 import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
 import { onAuthStateChanged } from 'firebase/auth';
 import { Provider } from 'react-redux';
 import RootScreen from './src/navigation/RootScreen';
@@ -86,7 +87,80 @@ const App = () => {
   return (
     <Provider store={store}>
       <StatusBar backgroundColor="blue" barStyle="dark-content" />
-      <RootScreen />
+      <NavigationContainer
+        linking={
+          // From https://docs.expo.dev/versions/latest/sdk/notifications/#handling-push-notifications-with-react-navigation
+          {
+            prefixes: ['danceblue://'],
+            config: {
+              screens: {
+                Main: {
+                  initialRouteName: 'Tab',
+                  screens: {
+                    Tab: {
+                      screens: {
+                        Home: 'redirect',
+                        Scoreboard: 'redirect/team-rankings',
+                        Team: 'redirect/my-team',
+                        Store: 'redirect/dancebluetique',
+                      },
+                    },
+                    Profile: 'redirect/app-profile',
+                    Notifications: 'redirect/app-notifications',
+                  },
+                },
+                DefaultRoute: '*',
+              },
+            },
+            async getInitialURL() {
+              // First, you may want to do the default deep link handling
+              // Check if app was opened from a deep link
+              let url = await Linking.getInitialURL();
+
+              if (url != null) {
+                return url;
+              }
+
+              // Handle URL from expo push notifications
+              const response = await Notifications.getLastNotificationResponseAsync();
+              url = response?.notification.request.content.data.url;
+
+              return url;
+            },
+            subscribe(listener) {
+              const onReceiveURL = ({ url }) => listener(url);
+
+              // Listen to incoming links from deep linking
+              const deepLinkSubscription = Linking.addEventListener('url', onReceiveURL);
+
+              // Listen to expo push notifications
+              const expoSubscription = Notifications.addNotificationResponseReceivedListener(
+                (response) => {
+                  const { url } = response.notification.request.content.data;
+
+                  // Any custom logic to see whether the URL needs to be handled
+                  // ...
+
+                  // Let React Navigation handle the URL
+                  listener(url);
+                }
+              );
+
+              return () => {
+                // Clean up the event listeners
+                if (deepLinkSubscription) {
+                  deepLinkSubscription.remove();
+                }
+                if (expoSubscription) {
+                  expoSubscription.remove();
+                }
+              };
+            },
+          }
+        }
+      >
+        <RootScreen />
+      </NavigationContainer>
     </Provider>
   );
 };
--- src/navigation/RootScreen.js
+++ src/navigation/RootScreen.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
-import { Linking } from 'react-native';
-import * as Notifications from 'expo-notifications';
-import { NavigationContainer } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useSelector } from 'react-redux';
 import SplashLogin from '../screens/Modals/SplashLogin';
@@ -21,78 +19,7 @@ const RootScreen = ({ navigation }) => {
   }, [navigation, isAuthLoaded, isLoggedIn]);
 
   return (
-    <NavigationContainer
-      linking={
-        // From https://docs.expo.dev/versions/latest/sdk/notifications/#handling-push-notifications-with-react-navigation
-        {
-          prefixes: ['danceblue://'],
-          config: {
-            screens: {
-              Main: {
-                initialRouteName: 'Tab',
-                screens: {
-                  Tab: {
-                    screens: {
-                      Home: 'redirect',
-                      Scoreboard: 'redirect/team-rankings',
-                      Team: 'redirect/my-team',
-                      Store: 'redirect/dancebluetique',
-                    },
-                  },
-                  Profile: 'redirect/app-profile',
-                  Notifications: 'redirect/app-notifications',
-                },
-              },
-              DefaultRoute: '*',
-            },
-          },
-          async getInitialURL() {
-            // First, you may want to do the default deep link handling
-            // Check if app was opened from a deep link
-            let url = await Linking.getInitialURL();
-
-            if (url != null) {
-              return url;
-            }
-
-            // Handle URL from expo push notifications
-            const response = await Notifications.getLastNotificationResponseAsync();
-            url = response?.notification.request.content.data.url;
-
-            return url;
-          },
-          subscribe(listener) {
-            const onReceiveURL = ({ url }) => listener(url);
-
-            // Listen to incoming links from deep linking
-            const deepLinkSubscription = Linking.addEventListener('url', onReceiveURL);
-
-            // Listen to expo push notifications
-            const expoSubscription = Notifications.addNotificationResponseReceivedListener(
-              (response) => {
-                const { url } = response.notification.request.content.data;
-
-                // Any custom logic to see whether the URL needs to be handled
-                // ...
-
-                // Let React Navigation handle the URL
-                listener(url);
-              }
-            );
-
-            return () => {
-              // Clean up the event listeners
-              if (deepLinkSubscription) {
-                deepLinkSubscription.remove();
-              }
-              if (expoSubscription) {
-                expoSubscription.remove();
-              }
-            };
-          },
-        }
-      }
-    >
+    <>
       <RootStack.Navigator>
         <RootStack.Screen name="Main" component={MainStackRoot} options={{ headerShown: false }} />
         <RootStack.Screen
@@ -106,7 +33,7 @@ const RootScreen = ({ navigation }) => {
           options={{ headerBackTitle: 'Back', headerTitle: 'DanceBlue' }}
         />
       </RootStack.Navigator>
-    </NavigationContainer>
+    </>
   );
 };
 
