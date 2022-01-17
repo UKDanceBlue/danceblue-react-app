9dc5d60eefb4e260f157a0ac1380009adaaca164
Tag Howard <tag.howard1080@gmail.com>
2022-01-16 22:28:01 -0500

    feat(CountdownView) Replace moment with date-fns

--- src/common/components/CountdownView.js
+++ src/common/components/CountdownView.js
@@ -1,43 +1,51 @@
 /* eslint-disable */
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text, ActivityIndicator } from 'react-native';
-import { collection, getDocs, where, query } from 'firebase/firestore';
-import moment from 'moment';
+import { intervalToDuration } from 'date-fns';
 
-import { firebaseFirestore } from '../FirebaseApp';
 import { useSelector } from 'react-redux';
 
 /**
  * A label for a unit of time
- * @param {string} props Properties of the component: value, label
+ * @param {number} value The numeric value to be shown on the top of this component
+ * @param {string} unit The unit (ENDING IN 's') of *value*
  * @returns A React Native component
  */
-const TimeUnit = ({ value, label }) => (
+const TimeUnit = ({ value, unit }) => (
   <View style={{ alignItems: 'center', paddingLeft: 7, paddingRight: 7 }}>
     <Text style={styles.countdownText}>{value}</Text>
-    <Text style={{ fontSize: 20, color: 'white' }}>{label}</Text>
+    <Text style={{ fontSize: 20, color: 'white' }}>
+      {value < 2 ? unit.substring(0, unit.length - 1) : unit}
+    </Text>
   </View>
 );
 
 const CountdownView = () => {
   const isConfigLoaded = useSelector((state) => state.appConfig.isConfigLoaded);
-  const countdownEventTimeStamp = useSelector((state) => state.appConfig.countdown.millis);
+  const countdownConfig = useSelector((state) => state.appConfig.countdown);
   const title = useSelector((state) => state.appConfig.countdown.title);
-  const [countdownDisplayTime, setCurrentTime] = useState(new Date());
+  const [countdownDisplayDuration, setCountdownDisplayDuration] = useState({
+    years: 0,
+    months: 0,
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
 
-  useEffect(
-    () =>
-      // 1 second timer
-      setInterval(
-        () =>
-          setCurrentTime(
-            // Create a date object with the difference between now and the countdown stored in firebase
-            new Date(Math.abs(countdownEventTimeStamp - new Date().getTime()))
-          ),
-        1000
-      ),
-    []
-  );
+  useEffect(() => {
+    // 1 second timer
+    const timer = setInterval(() => {
+      // Get time components
+      setCountdownDisplayDuration(
+        intervalToDuration({ start: new Date(), end: new Date(countdownConfig.millis) })
+      );
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -50,47 +58,12 @@ const CountdownView = () => {
             </Text>
           </View>
           <View style={styles.countdownTimer}>
-            {countdownDisplayTime.getMonth() !== 0 && (
-              <>
-                <TimeUnit
-                  label={countdownDisplayTime.getMonth() === 1 ? 'month' : 'months'}
-                  value={countdownDisplayTime.getMonth()}
-                />
+            {
+              // Check that this or any previous unit is nonzero
+              !!countdownDisplayDuration.years > 0 && (
+                <>
+                  <TimeUnit unit={'years'} value={countdownDisplayDuration.years} />
 
-                <Text
-                  style={{
-                    fontWeight: 'bold',
-                    fontSize: 40,
-                    color: 'white',
-                  }}
-                >
-                  :
-                </Text>
-              </>
-            )}
-            {(countdownDisplayTime.getDate() !== 0 && (
-              <>
-                <TimeUnit
-                  label={countdownDisplayTime.getDate() === 1 ? 'day' : 'days'}
-                  value={countdownDisplayTime.getDate()}
-                />
-                <Text
-                  style={{
-                    fontWeight: 'bold',
-                    fontSize: 40,
-                    color: 'white',
-                  }}
-                >
-                  :
-                </Text>
-              </>
-            )) ||
-              (countdownDisplayTime.getMonth() !== 0 && (
-                <>
-                  <TimeUnit
-                    label={countdownDisplayTime.getDate() === 1 ? 'day' : 'days'}
-                    value={countdownDisplayTime.getDate()}
-                  />
                   <Text
                     style={{
                       fontWeight: 'bold',
@@ -101,30 +74,14 @@ const CountdownView = () => {
                     :
                   </Text>
                 </>
-              ))}
-            {(countdownDisplayTime.getHours() !== 0 && (
-              <>
-                <TimeUnit
-                  label={countdownDisplayTime.getHours() === 1 ? 'hour' : 'hours'}
-                  value={countdownDisplayTime.getHours()}
-                />
-                <Text
-                  style={{
-                    fontWeight: 'bold',
-                    fontSize: 40,
-                    color: 'white',
-                  }}
-                >
-                  :
-                </Text>
-              </>
-            )) ||
-              ((countdownDisplayTime.getDay() !== 0 || countdownDisplayTime.getMonth() !== 0) && (
+              )
+            }
+            {
+              // Check that this or any previous unit is nonzero
+              !!countdownDisplayDuration.years + !!countdownDisplayDuration.months > 0 && (
                 <>
-                  <TimeUnit
-                    label={countdownDisplayTime.getHours() === 1 ? 'hour' : 'hours'}
-                    value={countdownDisplayTime.getHours()}
-                  />
+                  <TimeUnit unit={'months'} value={countdownDisplayDuration.months} />
+
                   <Text
                     style={{
                       fontWeight: 'bold',
@@ -135,32 +92,17 @@ const CountdownView = () => {
                     :
                   </Text>
                 </>
-              ))}
-            {(countdownDisplayTime.getMinutes() !== 0 && (
-              <>
-                <TimeUnit
-                  label={countdownDisplayTime.getMinutes() === 1 ? 'min' : 'mins'}
-                  value={countdownDisplayTime.getMinutes()}
-                />
-                <Text
-                  style={{
-                    fontWeight: 'bold',
-                    fontSize: 40,
-                    color: 'white',
-                  }}
-                >
-                  :
-                </Text>
-              </>
-            )) ||
-              ((countdownDisplayTime.getHours() !== 0 ||
-                countdownDisplayTime.getDay() !== 0 ||
-                countdownDisplayTime.getMonth() !== 0) && (
+              )
+            }
+            {
+              // Check that this or any previous unit is nonzero
+              !!countdownDisplayDuration.years +
+                !!countdownDisplayDuration.months +
+                !!countdownDisplayDuration.days >
+                0 && (
                 <>
-                  <TimeUnit
-                    label={countdownDisplayTime.getMinutes() === 1 ? 'min' : 'mins'}
-                    value={countdownDisplayTime.getMinutes()}
-                  />
+                  <TimeUnit unit={'days'} value={countdownDisplayDuration.days} />
+
                   <Text
                     style={{
                       fontWeight: 'bold',
@@ -171,22 +113,67 @@ const CountdownView = () => {
                     :
                   </Text>
                 </>
-              ))}
-            {(countdownDisplayTime.getSeconds() !== 0 && (
-              <TimeUnit
-                label={countdownDisplayTime.getSeconds() === 1 ? 'sec' : 'secs'}
-                value={countdownDisplayTime.getSeconds()}
-              />
-            )) ||
-              ((countdownDisplayTime.getMonth() !== 0 ||
-                countdownDisplayTime.getHours() !== 0 ||
-                countdownDisplayTime.getDay() !== 0 ||
-                countdownDisplayTime.getMonth() !== 0) && (
-                <TimeUnit
-                  label={countdownDisplayTime.getSeconds() === 1 ? 'sec' : 'secs'}
-                  value={countdownDisplayTime.getSeconds()}
-                />
-              ))}
+              )
+            }
+            {
+              // Check that this or any previous unit is nonzero
+              !!countdownDisplayDuration.years +
+                !!countdownDisplayDuration.months +
+                !!countdownDisplayDuration.days +
+                !!countdownDisplayDuration.hours >
+                0 && (
+                <>
+                  <TimeUnit unit={'hours'} value={countdownDisplayDuration.hours} />
+
+                  <Text
+                    style={{
+                      fontWeight: 'bold',
+                      fontSize: 40,
+                      color: 'white',
+                    }}
+                  >
+                    :
+                  </Text>
+                </>
+              )
+            }
+            {
+              // Check that this or any previous unit is nonzero
+              !!countdownDisplayDuration.years +
+                !!countdownDisplayDuration.months +
+                !!countdownDisplayDuration.days +
+                !!countdownDisplayDuration.hours +
+                !!countdownDisplayDuration.minutes >
+                0 && (
+                <>
+                  <TimeUnit unit={'minutes'} value={countdownDisplayDuration.minutes} />
+
+                  <Text
+                    style={{
+                      fontWeight: 'bold',
+                      fontSize: 40,
+                      color: 'white',
+                    }}
+                  >
+                    :
+                  </Text>
+                </>
+              )
+            }
+            {
+              // Check that this or any previous unit is nonzero
+              !!countdownDisplayDuration.years +
+                !!countdownDisplayDuration.months +
+                !!countdownDisplayDuration.days +
+                !!countdownDisplayDuration.hours +
+                !!countdownDisplayDuration.minutes +
+                !!countdownDisplayDuration.seconds >
+                0 && (
+                <>
+                  <TimeUnit unit={'seconds'} value={countdownDisplayDuration.seconds} />
+                </>
+              )
+            }
           </View>
         </>
       )}
