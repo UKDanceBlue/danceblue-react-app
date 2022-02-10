1efdcaeb86edc685baa401cad9edac377cd94f70
Tag Howard <tag.howard1080@gmail.com>
2022-02-10 13:35:34 -0500

    Add donate button to home

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { SafeAreaView, ScrollView } from 'react-native';
+import { Linking, SafeAreaView, ScrollView } from 'react-native';
 import { useAppSelector } from '../../common/CustomHooks';
 import SponsorCarousel from './SponsorCarousel';
 import CountdownView from '../../common/components/CountdownView';
 import HeaderImage from './HeaderImage';
+import { Button } from 'react-native-elements';
 
 /**
  * Component for home screen in main navigation
@@ -14,13 +15,26 @@ const HomeScreen = () => {
   const isConfigLoaded = useAppSelector((state) => state.appConfig.isConfigLoaded);
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <HeaderImage />
-        {isConfigLoaded && countdown && <CountdownView />}
-        <SponsorCarousel />
-      </SafeAreaView>
-    </ScrollView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <HeaderImage />
+      {isConfigLoaded && countdown && <CountdownView />}
+      <Button
+        style={{ margin: 10, alignSelf: 'center' }}
+        onPress={() => {
+          if (
+            Linking.canOpenURL(
+              'https://danceblue.networkforgood.com/causes/4789-danceblue-golden-matrix-fund-dance-teams?utm_source=website&utm_medium=unit_website'
+            )
+          ) {
+            Linking.openURL(
+              'https://danceblue.networkforgood.com/causes/4789-danceblue-golden-matrix-fund-dance-teams?utm_source=website&utm_medium=unit_website'
+            );
+          }
+        }}
+        title="Donate!"
+      />
+      <SponsorCarousel />
+    </SafeAreaView>
   );
 };
 
