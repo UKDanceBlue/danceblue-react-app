83c9e87143e6f92f10f90a35aaab4e938e912d07
Tag Howard <tag.howard1080@gmail.com>
2022-02-22 18:17:35 -0500

    Convert HourActivities to a map temporarily

--- src/screens/HoursScreen/HourActivities.tsx
+++ src/screens/HoursScreen/HourActivities.tsx
@@ -3,44 +3,24 @@ import { Text, View } from 'react-native';
 import { Input } from 'react-native-elements';
 import { firebaseFirestore } from '../../common/FirebaseApp';
 
-export default [
-  <Text key={0}>This is dynamic content loaded from HourActivities.tsx</Text>,
-  <View key={1}>
-    <Input
-      autoCompleteType="off"
-      autoComplete="off"
-      onSubmitEditing={async (event) => {
-        const submittedText = event.nativeEvent.text;
-        const parsedText = parseInt(submittedText, 10);
+export default {
+  test: <Text key={0}>This is dynamic content loaded from HourActivities.tsx</Text>,
+  'guessing-game': (
+    <View key={1}>
+      <Input
+        autoCompleteType="off"
+        autoComplete="off"
+        onSubmitEditing={async (event) => {
+          const submittedText = event.nativeEvent.text;
+          const parsedText = parseInt(submittedText, 10);
 
-        if (parsedText && typeof parsedText === 'number' && !Number.isNaN(parsedText)) {
-          await addDoc(collection(firebaseFirestore, 'marathon/2022/guessing-game'), {
-            guess: parsedText,
-          });
-        }
-      }}
-    />
-  </View>,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-];
+          if (parsedText && typeof parsedText === 'number' && !Number.isNaN(parsedText)) {
+            await addDoc(collection(firebaseFirestore, 'marathon/2022/guessing-game'), {
+              guess: parsedText,
+            });
+          }
+        }}
+      />
+    </View>
+  ),
+} as { [key: string]: JSX.Element };
