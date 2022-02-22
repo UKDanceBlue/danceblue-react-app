eb85d38cdba6298138e44303b4967f2873e2fadb
Tag Howard <tag.howard1080@gmail.com>
2022-02-22 13:51:07 -0500

    Remove old comment

--- src/common/CustomHooks.ts
+++ src/common/CustomHooks.ts
@@ -45,6 +45,3 @@ export function useCurrentDate(refreshInterval: number) {
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-// HEY TAG!!!!!!!!
-// READ THIS THING: https://redux-toolkit.js.org/usage/usage-with-typescript
