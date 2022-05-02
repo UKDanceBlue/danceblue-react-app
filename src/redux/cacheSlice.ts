6c81316cbc6b540609c53396dc5e4bc1f4b9e48f
Tag Howard <tag.howard1080@gmail.com>
2022-05-02 17:17:46 -0400

    Start rewrite of redux slices

---
+++ src/redux/cacheSlice.ts
@@ -0,0 +1,17 @@
+import { type } from 'os';
+import { FirestoreNotification } from '../types/FirebaseTypes';
+
+type CachedData<DataType> = {
+  data: DataType;
+  expireAfter?: Date;
+  // We may have data that should be refreshed, but doesn't need to be this is a case for refreshAfter
+  refreshAfter?: Date;
+  // Omitting both timeouts indicated the data never expires, this should be avoided
+};
+
+type CacheSliceType = {
+  cachedNotification: { [key: string]: CachedData<FirestoreNotification> };
+  cachedImage: { [key: string]: CachedData<string> };
+};
+
+const initialState = {};
---
+++ src/redux/userDataSlice.ts
@@ -0,0 +1,16 @@
+export type UserLoginType = 'anonymous' | 'ms-oath-linkblue';
+type UserDataType = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  uid: string;
+  linkBlue?: string;
+  attributes: {
+    [key: string]: string;
+  };
+  authClaims: {
+    [key: string]: string;
+  };
+  userLoginType: UserLoginType;
+  pastNotificationIds: string[];
+};
