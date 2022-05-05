810556dabb7d4ed6688fdaf9912fed52e45e7ff2
Tag Howard <tag.howard1080@gmail.com>
2022-05-05 15:57:13 -0400

    Fix jest

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  preset: 'jest-expo',
+  preset: "jest-expo",
   globals: {
-    'ts-jest': {
+    "ts-jest": {
       tsconfig: {
-        jsx: 'react',
+        jsx: "react-jsx",
       },
     },
   },
   transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js",
   },
-  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+  testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
   collectCoverageFrom: [
-    '**/*.{ts,tsx}',
-    '!**/coverage/**',
-    '!**/node_modules/**',
-    '!**/babel.config.js',
-    '!**/jest.setup.js',
+    "**/*.{ts,tsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/jest.setup.js",
   ],
-  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  moduleFileExtensions: ["js", "ts", "tsx"],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base)',
+    "node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base)",
   ],
-  coverageReporters: ['json-summary', 'text', 'lcov'],
+  coverageReporters: ["json-summary", "text", "lcov"],
 };
