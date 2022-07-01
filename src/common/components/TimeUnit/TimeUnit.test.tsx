2b9f164f06c0df6dceb7904ae4b3d3eb89881323
Tag Howard <tag.howard1080@gmail.com>
2022-06-30 20:18:18 -0400

    Update tests to support NativeBase

--- src/common/components/TimeUnit/TimeUnit.test.tsx
+++ src/common/components/TimeUnit/TimeUnit.test.tsx
@@ -1,21 +1,21 @@
-import { render } from "@testing-library/react-native";
+import { renderWithNativeBase } from "../../test-helpers/NativeBase";
 
 import TimeUnit from ".";
 
 describe("<TimeUnit />", () => {
   it("renders correctly with a singular unit", () => {
-    const tree = render(<TimeUnit value={1} unit={"seconds"} />).toJSON() as unknown;
+    const tree = renderWithNativeBase(<TimeUnit value={1} unit={"seconds"} />).toJSON() as unknown;
 
     expect(tree).toMatchSnapshot();
   });
   it("renders correctly with a plural unit", () => {
-    const tree = render(<TimeUnit value={10} unit={"seconds"} />).toJSON() as unknown;
+    const tree = renderWithNativeBase(<TimeUnit value={10} unit={"seconds"} />).toJSON() as unknown;
 
     expect(tree).toMatchSnapshot();
   });
 
   it("renders 0 with a negative value", () => {
-    const tree = render(<TimeUnit value={-1} unit={"seconds"} />);
+    const tree = renderWithNativeBase(<TimeUnit value={-1} unit={"seconds"} />);
 
     const valueElement = tree.queryAllByText("0");
     expect(valueElement).toHaveLength(1);
@@ -26,7 +26,7 @@ describe("<TimeUnit />", () => {
 
   it("renders 0 with a nullish value", () => {
     // @ts-expect-error Testing nullish values
-    const tree = render(<TimeUnit value={null} unit={"seconds"} />);
+    const tree = renderWithNativeBase(<TimeUnit value={null} unit={"seconds"} />);
 
     const valueElement = tree.queryAllByText("0");
     expect(valueElement).toHaveLength(1);
@@ -40,7 +40,7 @@ describe("<TimeUnit />", () => {
 
     const invalidUnit = "an invalid unit";
     // @ts-expect-error Testing an invalid unit
-    expect(() => render(<TimeUnit value={1} unit={invalidUnit} />)).toThrow(`Invalid unit: ${invalidUnit}`);
+    expect(() => renderWithNativeBase(<TimeUnit value={1} unit={invalidUnit} />)).toThrow(`Invalid unit: ${invalidUnit}`);
 
     mockedConsoleError.mockRestore();
   });
