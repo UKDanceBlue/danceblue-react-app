dfc97fbd5aeb8627f1320c1effdb90a8e0bddbcf
Tag Howard <tag.howard1080@gmail.com>
2022-07-10 02:55:26 -0400

    Use a SectionList for EventListScreen

--- src/navigation/root/tab/EventListScreen/EventListScreen.tsx
+++ src/navigation/root/tab/EventListScreen/EventListScreen.tsx
@@ -1,9 +1,9 @@
 import firebaseFirestore from "@react-native-firebase/firestore";
 import { useNavigation } from "@react-navigation/native";
 import { DateTime, Interval } from "luxon";
-import { Divider, Heading, ScrollView, useTheme } from "native-base";
-import { useEffect, useState } from "react";
-import { SafeAreaView, TouchableOpacity } from "react-native";
+import { Center, Heading, SectionList, useTheme } from "native-base";
+import { useCallback, useEffect, useState } from "react";
+import { TouchableOpacity } from "react-native";
 
 
 import { useColorModeValue } from "../../../../common/CustomHooks";
@@ -22,10 +22,12 @@ const EventListScreen = () => {
   const [ events, setEvents ] = useState<ParsedEvent[]>([]);
   const [ today, setToday ] = useState<ParsedEvent[]>([]);
   const [ upcoming, setUpcoming ] = useState<ParsedEvent[]>([]);
+  const [ refreshing, setRefreshing ] = useState(false);
+
   const navigation = useNavigation<TabNavigatorProps<"Events">["navigation"]>();
 
-  useEffect(() => {
-    let componentUnmounted = false;
+  const refresh = useCallback(() => {
+    setRefreshing(true);
     const firestoreEvents: ParsedEvent[] = [];
     void fbFirestore.collection("events").where("endTime", ">", firebaseFirestore.Timestamp.now())
       .get()
@@ -36,15 +38,17 @@ const EventListScreen = () => {
             firestoreEvents.push(await parseFirestoreEvent(data, fbStorage));
           }));
 
-          if (!componentUnmounted) {
-            setEvents(firestoreEvents);
-          }
+          setEvents(firestoreEvents);
         }
-      );
-    return () => {
-      componentUnmounted = true;
-    };
+      )
+      .finally(() => setRefreshing(false));
   }, [ fbFirestore, fbStorage ]);
+
+  useEffect(() => {
+    refresh();
+  }, [
+    fbFirestore, fbStorage, refresh
+  ]);
 
   /**
    * Splits *events* into *today* and *upcoming* based on the events' start day
@@ -69,58 +73,50 @@ const EventListScreen = () => {
    * Called by React Native when rendering the screen
    */
   return (
-    <ScrollView backgroundColor={screenBackgroundColor}>
-      <SafeAreaView>
-        {
-          today.length >= 1 &&
-          <>
-            <Heading fontSize="3xl" m="1">
-            Today&apos;s Events
-            </Heading>
-            <Divider/>
-          </>
+    <SectionList
+      backgroundColor={screenBackgroundColor}
+      height="100%"
+      onRefresh={refresh}
+      refreshing={refreshing}
+      sections={
+        [
+          {
+            title: "Today",
+            data: today
+          }, {
+            title: "Upcoming",
+            data: upcoming
+          }
+        ]
+      }
+      renderSectionHeader={({
+        section: {
+          title, data
         }
-        {today.map((row, index) => (
-          <TouchableOpacity
-            onPress={() => navigation.navigate("Event", { event: row })}
-            key={index}
-          >
-            <EventRow
-              key={index}
-              title={row.title}
-              blurb={`${row.description.substring(0, 100) }...`}
-              interval={row.interval}
-              imageSource={{ uri: row.image?.url, width: row.image?.width, height: row.image?.height }}
-            />
-          </TouchableOpacity>
-        ))}
-        {
-          upcoming.length >= 1 &&
-          <>
-            <Heading fontSize="3xl" m="1">
-            Upcoming Events
-            </Heading>
-            <Divider/>
-          </>
-        }
-        {
-          upcoming.map((row, index) => (
-            <TouchableOpacity
-              onPress={() => navigation.navigate("Event", { event: row })}
-              key={index}
-            >
-              <EventRow
-                key={index}
-                title={row.title}
-                blurb={`${row.description.substring(0, 100) }...`}
-                interval={row.interval}
-                imageSource={{ uri: row.image?.url, width: row.image?.width, height: row.image?.height }}
-              />
-            </TouchableOpacity>
-          ))
-        }
-      </SafeAreaView>
-    </ScrollView>
+      }) => data.length > 0 ? (
+        <Center
+          backgroundColor={screenBackgroundColor}
+          borderBottomWidth={"1"}
+          borderBottomRadius={"xl"}
+          shadow={"1"}>
+          <Heading fontSize="xl" my={"2"}>
+            {title}
+          </Heading>
+        </Center>
+      ) : <></>}
+      renderItem={({ item: row }) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Event", { event: row })}
+        >
+          <EventRow
+            title={row.title}
+            blurb={`${row.description.substring(0, 100) }...`}
+            interval={row.interval}
+            imageSource={{ uri: row.image?.url, width: row.image?.width, height: row.image?.height }}
+          />
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
