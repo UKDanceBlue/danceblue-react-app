9a5fbd8a9bac8403f16664e6876154231635e4bd
Tag Howard <tag.howard1080@gmail.com>
2022-08-11 21:53:16 -0400

    Fix deps

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "module-resolver",
-        {alias: {"@native-base/icons": "@native-base/icons/lib",},},
-      ],
-    ],
+    plugins: [["module-resolver",],],
   };
 };
