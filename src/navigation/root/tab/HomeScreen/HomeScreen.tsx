bca08e1340a26695086431a4141a06f0b4c3d731
Tag Howard <tag.howard1080@gmail.com>
2022-10-27 11:39:50 -0400

    Revert test page

--- src/navigation/root/tab/HomeScreen/HomeScreen.tsx
+++ src/navigation/root/tab/HomeScreen/HomeScreen.tsx
@@ -1,7 +1,12 @@
-import { ScrollView, VStack, useTheme } from "native-base";
+import { Box, Button, Text, VStack, useTheme } from "native-base";
+import { Linking } from "react-native";
 
-import NativeBaseMarkdown from "../../../../common/components/NativeBaseMarkdown";
+import PodcastPlayer from "../../../../common/components/PodcastPlayer";
 import { useColorModeValue } from "../../../../common/customHooks";
+import { universalCatch } from "../../../../common/logging";
+
+import HeaderImage from "./HeaderImage";
+import SponsorCarousel from "./SponsorCarousel";
 
 /**
  * Component for home screen in main navigation
@@ -12,160 +17,40 @@ const HomeScreen = () => {
 
   return (
     <VStack flexDirection="column" bgColor={bgColor}>
-      <ScrollView mx="5">
-        <NativeBaseMarkdown>
-          {`Headings
-
-# h1 Heading
-## h2 Heading
-### h3 Heading
-#### h4 Heading
-##### h5 Heading
-###### h6 Heading
-
-
-Horizontal Rules
-
-Some text above
-___
-
-Some text in the middle
-
----
-
-Some text below
-
-
-Emphasis
-
-**This is bold text**
-
-__This is bold text__
-
-*This is italic text*
-
-_This is italic text_
-
-~~Strikethrough~~
-
-
-Blockquotes
-
-> Blockquotes can also be nested...
->> ...by using additional greater-than signs right next to each other...
-> > > ...or with spaces between arrows.
-
-
-Lists
-
-Unordered
-
-+ Create a list by starting a line with \`+\`, \`-\`, or \`*\`
-+ Sub-lists are made by indenting 2 spaces:
-  - Marker character change forces new list start:
-    * Ac tristique libero volutpat at
-    + Facilisis in pretium nisl aliquet. This is a very long list item that will surely wrap onto the next line.
-    - Nulla volutpat aliquam velit
-+ Very easy!
-
-Ordered
-
-1. Lorem ipsum dolor sit amet
-2. Consectetur adipiscing elit. This is a very long list item that will surely wrap onto the next line.
-3. Integer molestie lorem at massa
-
-Start numbering with offset:
-
-57. foo
-58. bar
-
-
-Code
-
-Inline \`code\`
-
-Indented code
-
-    // Some comments
-    line 1 of code
-    line 2 of code
-    line 3 of code
-
-
-Block code "fences"
-
-\`\`\`
-Sample text here...
-\`\`\`
-
-Syntax highlighting
-
-\`\`\` js
-var foo = function (bar) {
-  return bar++;
-};
-
-console.log(foo(5));
-\`\`\`
-
-
-Tables
-
-| Option | Description |
-| ------ | ----------- |
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
-
-Right aligned columns
-
-| Option | Description |
-| ------:| -----------:|
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
-
-
-Links
-
-[link text](https://www.google.com)
-
-[link with title](https://www.google.com "title text!")
-
-Autoconverted link https://www.google.com (enable linkify to see)
-
-[internal link](danceblue://events)
-
-
-Images
-
-![Minion](https://octodex.github.com/images/minion.png)
-![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
-
-Like links, Images also have a footnote style syntax
-
-![Alt text][id]
-
-With a reference later in the document defining the URL location:
-
-[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
-
-
-Typographic Replacements
-
-Enable typographer option to see result.
-
-(c) (C) (r) (R) (tm) (TM) (p) (P) +-
-
-test.. test... test..... test?..... test!....
-
-!!!!!! ???? ,,  -- ---
-
-"Smartypants, double quotes" and 'single quotes'
-
-`}
-        </NativeBaseMarkdown>
-      </ScrollView>
+      <Box height="35%" tintColor={bgColor}>
+        <HeaderImage />
+      </Box>
+      <Box height="10%">
+        <Button
+          borderRadius={0}
+          margin={0}
+          backgroundColor={colors.blue[700]}
+          _pressed={{ opacity: 0.5 }}
+          onPress={async () => {
+            if (
+              await Linking.canOpenURL(
+                "https://danceblue.networkforgood.com/"
+              ).catch(universalCatch)
+            ) {
+              Linking.openURL(
+                "https://danceblue.networkforgood.com/"
+              ).catch(universalCatch);
+            }
+          }}
+        >
+          <Text
+            bold
+            fontSize={30}
+            color="light.100"
+            shadow="1">Donate Now!</Text>
+        </Button>
+      </Box>
+      <Box height="15%">
+        <PodcastPlayer />
+      </Box>
+      <Box height="40%">
+        <SponsorCarousel />
+      </Box>
     </VStack>
   );
 };
