1f98511750d5fea47abb017f13a92b468d83dfcb
Jacksonnnnn <jhuse01@gmail.com>
2023-01-19 14:28:55 -0500

    Quick Fixes for Sponsors Block

--- src/common/components/SponsorsBlock/SponsorsBlock.tsx
+++ src/common/components/SponsorsBlock/SponsorsBlock.tsx
@@ -1,16 +1,18 @@
-import { Box, Heading, ScrollView, Text, useTheme, View } from "native-base";
-import { StyleSheet } from "react-native";
-import { useThemeColors } from "../../customHooks";
 import firebaseFirestore from "@react-native-firebase/firestore";
+import { Box, Heading, ScrollView, Text, View, useTheme } from "native-base";
 import { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
 
-import { universalCatch } from "../../logging";
 import { LegacyFirestoreSponsor } from "../../../types/FirestoreSponsor";
+import { useThemeColors } from "../../customHooks";
+import { universalCatch } from "../../logging";
 import SponsorCard from "../SponsorCard";
 
 const SponsorsBlock = () => {
   const themes = useTheme();
-  const {primary, secondary, tertiary, success, warning, error, danger} = useThemeColors();
+  const {
+    primary, secondary, tertiary, success, warning, error, danger
+  } = useThemeColors();
   const [ sponsors, setSponsors ] = useState<LegacyFirestoreSponsor[]>([]);
 
   useEffect(() => {
@@ -36,7 +38,8 @@ const SponsorsBlock = () => {
       name={sponsor.name ?? ""}
       imagePath={sponsor.logo ?? ""}
       sponsorLink={sponsor.link}
-      key={i} descrption={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}    />
+      key={i}
+      descrption={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."} />
   ));
   
   return (
@@ -45,29 +48,29 @@ const SponsorsBlock = () => {
         {cards}
       </Box>
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
-    backgroundColor: '#fff',
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
     padding: 20,
     height: 50,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
   top: {
     flex: 1,
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
   },
   middle: {
     flex: 1,
-    backgroundColor: 'beige',
+    backgroundColor: "beige",
   },
   bottom: {
     flex: 1,
-    backgroundColor: 'pink',
+    backgroundColor: "pink",
   },
 });
 
