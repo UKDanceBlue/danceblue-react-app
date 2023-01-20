bafb45354a13f4bdd816fbe40bc21c32a8d09623
Jacksonnnnn <jhuse01@gmail.com>
2023-01-20 00:44:40 -0500

    Scoreboard

--- src/common/components/Scoreboard/Scoreboard.tsx
+++ src/common/components/Scoreboard/Scoreboard.tsx
@@ -1,8 +1,9 @@
-import { Text, useTheme } from "native-base";
+import { Text, View, useTheme } from "native-base";
 
 import { useThemeColors, useThemeFonts } from "../../customHooks";
+import ScoreboardItem from "../ScoreboardItem";
 
-const Scoreboard = ({ param }: { param:string }) => {
+const Scoreboard = ({ title }: { title:string }) => {
   const themes = useTheme();
   const {
     primary, secondary, tertiary, success, warning, error, danger
@@ -10,9 +11,47 @@ const Scoreboard = ({ param }: { param:string }) => {
   const {
     heading, body, mono
   } = useThemeFonts();
+  const ichooseyou = "awareness-ribbon";
+  const fromyou = "entypo";
 
   return (
-    <Text variant="fun" bg={primary[700]} color="#fff">hi {param}</Text>
+    <View>
+      <Text
+        textAlign="center"
+        color={secondary[400]}
+        fontFamily="bodoni-flf-bold"
+        fontSize={themes.fontSizes["4xl"]}
+        style={{
+          textShadowColor: secondary[300],
+          textShadowOffset: { width: 2, height: 1.5 },
+          textShadowRadius: 1
+        }}>{title}</Text>
+    
+      <ScoreboardItem
+        rank={1}
+        name="Alpha Gamma Delta <3"
+        points={1000}
+        icon={ichooseyou}
+        type={fromyou}/>
+      <ScoreboardItem
+        rank={2}
+        name="Alpha Gamma Delta <3"
+        points={1000}
+        icon={ichooseyou}
+        type={fromyou}/>
+      <ScoreboardItem
+        rank={3}
+        name="Alpha Gamma Delta <3"
+        points={1000}
+        icon={ichooseyou}
+        type={fromyou}/>
+      <ScoreboardItem
+        rank={4}
+        name="Alpha Gamma Delta <3"
+        points={1000}
+        icon={ichooseyou}
+        type={fromyou}/>
+    </View>
   );
 };
 export default Scoreboard;
