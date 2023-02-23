5d736eba86a0693b694f8bd0fcfd21341610b4a0
Jacksonnnnn <jhuse01@gmail.com>
2023-02-23 13:15:49 -0500

    Tapping on "DanceBlue" takes user to website.

--- src/navigation/root/tab/DBHeaderText.tsx
+++ src/navigation/root/tab/DBHeaderText.tsx
@@ -1,10 +1,23 @@
 import { Text } from "native-base";
-import { useWindowDimensions } from "react-native";
+import { Linking, useWindowDimensions } from "react-native";
+
+import { universalCatch } from "../../../common/logging";
 
 export function DBHeaderText() {
   const { width } = useWindowDimensions();
 
   return (<Text
+    onPress={async () => {
+      if (
+        await Linking.canOpenURL(
+          "https://www.danceblue.org/"
+        ).catch(universalCatch)
+      ) {
+        Linking.openURL(
+          "https://www.danceblue.org/"
+        ).catch(universalCatch);
+      }
+    }}
     marginLeft={ Math.round(width * 0.025)}
     fontSize="3xl"
     color="primary.600"
