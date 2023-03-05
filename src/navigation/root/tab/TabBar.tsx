32e0c63e52b25807d98b43d384135503aba46850
Joshua Tag Howard <tag@jthoward.dev>
2023-03-05 15:26:08 -0500

    Add a basic fancy button navigation

--- src/navigation/root/tab/TabBar.tsx
+++ src/navigation/root/tab/TabBar.tsx
@@ -1,11 +1,8 @@
 // Import third-party dependencies
-import { FontAwesome5 } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { RouteProp } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Image } from "native-base";
 import { ReactElement, useEffect, useState } from "react";
-import { ImageSourcePropType, useWindowDimensions } from "react-native";
 
 // Import first-party dependencies
 import { log } from "../../../common/logging";
@@ -17,6 +14,7 @@ import { DBHeaderText } from "./DBHeaderText";
 import EventListScreen from "./EventListScreen";
 import HomeScreen from "./HomeScreen";
 import HoursScreen from "./HoursScreen";
+import TabBarComponent from "./TabBarComponent";
 import SpiritScreen from "./spirit/SpiritStack";
 
 const Tabs = createBottomTabNavigator<TabNavigatorParamList>();
@@ -34,9 +32,6 @@ const TabBar = () => {
   const {
     isConfigLoaded, enabledScreens
   } = useAppConfig();
-  const {
-    width, height
-  } = useWindowDimensions();
 
   const [ currentTabs, setCurrentTabs ] = useState<ReactElement[]>([]);
 
@@ -54,46 +49,18 @@ const TabBar = () => {
 
   return (
     <Tabs.Navigator
-      screenOptions={({
-        route, navigation
-      }: {
+      screenOptions={({ navigation }: {
             navigation: NativeStackNavigationProp<RootStackParamList>;
             route: RouteProp<TabNavigatorParamList>;
         }) => ({
-        tabBarIcon: ({
-          color, size
-        }) => {
-          const iconMap = {
-            // https://icons.expo.fyi/
-            // Key: Screen   Value: Icon ID
-            Home: "home",
-            Events: "calendar",
-            Store: "store",
-            More: "ellipsis-h",
-            Scoreboard: "list-ol",
-            Teams: "users",
-            Donate: "hand-holding-heart",
-            Marathon: "people-arrows",
-            "Scavenger Hunt": "search"
-          };
 
-          // You can return any component that you like here!
-          return (
-            <FontAwesome5
-              name={iconMap[route.name]}
-              size={size}
-              color={color}
-              style={{ textAlignVertical: "center" }}
-            />
-          );
-        },
-        tabBarBackground: () => (
-          <Image
-            source={require("../../../../assets/screens/navigation/standardBG.png") as ImageSourcePropType}
-            alt="Navigation Background Image"
-            width={width}
-            height={130}/>
-        ),
+        // tabBarBackground: () => (
+        //   <Image
+        //     source={require("../../../../assets/screens/navigation/standardBG.png") as ImageSourcePropType}
+        //     alt="Navigation Background Image"
+        //     width={width}
+        //     height={130}/>
+        // ),
         headerLeft: DBHeaderText,
         headerTitle: () => null,
         headerRight: () => (
@@ -118,7 +85,7 @@ const TabBar = () => {
           }, null
         ],
       })}
-      // tabBar={(props) => (<TabBarComponent {...props} />)}
+      tabBar={(props) => (<TabBarComponent {...props} />)}
     >
       <Tabs.Screen name="Home" component={HomeScreen} />
       {isConfigLoaded && currentTabs}
--- src/navigation/root/tab/TabBarComponent.tsx
+++ src/navigation/root/tab/TabBarComponent.tsx
@@ -1,62 +1,140 @@
+import { FontAwesome5 } from "@expo/vector-icons";
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
-import { Text, TouchableOpacity, View } from "react-native";
+import { VStack } from "native-base";
+import { ZStack } from "native-base/src/components/primitives";
+import { Text, TouchableOpacity, View, useWindowDimensions } from "react-native";
+
+import BackgroundCutout from "../../../../assets/screens/navigation/background-cutout";
 
 // From https://reactnavigation.org/docs/bottom-tab-navigator#tabbar
 
-function TabBarComponent({
-  state, descriptors, navigation
-}: BottomTabBarProps) {
+const iconMap = {
+  // https://icons.expo.fyi/
+  // Key: Screen   Value: Icon ID
+  Home: "home",
+  Events: "calendar",
+  Store: "store",
+  More: "ellipsis-h",
+  Scoreboard: "list-ol",
+  Teams: "users",
+  Donate: "hand-holding-heart",
+  Marathon: "people-arrows",
+  "Scavenger Hunt": "search"
+};
+
+const tabBarIcon = ({
+  color, size, routeName
+}: { color: string; size: number; routeName: keyof typeof iconMap }) => {
+  // You can return any component that you like here!
   return (
-    <View style={{ flexDirection: "row" }}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const label = typeof options.tabBarLabel === "string"
-          ? options.tabBarLabel
-          : options.title !== undefined
-            ? options.title
-            : route.name;
+    <FontAwesome5
+      name={iconMap[routeName]}
+      size={size}
+      color={color}
+      style={{ textAlignVertical: "center" }}
+    />
+  );
+};
 
-        const isFocused = state.index === index;
+function TabBarComponent({
+  state, descriptors, navigation, insets
+}: BottomTabBarProps) {
+  const {
+    height: screenHeight, width: screenWidth
+  } = useWindowDimensions();
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: "tabPress",
-            target: route.key,
-            canPreventDefault: true,
-          });
+  const tabBarHeight = screenHeight*.1;
 
-          if (!isFocused && !event.defaultPrevented) {
+  return (
+    <ZStack height={tabBarHeight} width={screenWidth} style={{ marginTop: insets.top, marginBottom: insets.bottom, marginLeft: insets.left, marginRight: insets.right }}>
+      <BackgroundCutout svgProps={{ width: screenWidth, height: tabBarHeight }} color="#ededed" />
+      <View style={{ flexDirection: "row", width: screenWidth, height: tabBarHeight }}>
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key];
+          const label = typeof options.tabBarLabel === "string"
+            ? options.tabBarLabel
+            : options.title !== undefined
+              ? options.title
+              : route.name;
+
+          const isFocused = state.index === index;
+
+          const isMiddle = state.routes.length % 2 === 1 && index === Math.floor(state.routes.length / 2);
+
+          const sizeOfIcon = tabBarHeight*.32;
+
+          const onPress = () => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!isFocused && !event.defaultPrevented) {
             // The `merge: true` option makes sure that the params inside the tab screen are preserved
-            navigation.navigate({ name: route.name, merge: true, params: {} });
+              navigation.navigate({ name: route.name, merge: true, params: {} });
+            }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: "tabLongPress",
+              target: route.key,
+            });
+          };
+
+          const ButtonComponent = () => (
+            <TouchableOpacity
+              accessibilityRole="button"
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={{
+                flex: 1,
+                width: screenWidth*.1*1.4,
+                height: screenWidth*.1*1.4,
+                borderRadius: screenWidth*.1*1.4,
+                backgroundColor: isMiddle ? "#673ab7" : undefined,
+              }}
+            >
+              <VStack
+                alignItems="center"
+                justifyContent="center"
+                flex={1}>
+                {tabBarIcon({ color: isFocused ? "#673ab7" : "#222", size: sizeOfIcon, routeName: route.name as keyof typeof iconMap })}
+                <Text style={{ color: isFocused ? "#673ab7" : "#222" }}>
+                  {label}
+                </Text>
+              </VStack>
+            </TouchableOpacity>
+          );
+
+          let ButtonComponentToShow = ButtonComponent;
+          if (isMiddle) {
+            const WrappedButtonComponent = () => (
+              <View style={{
+                position: "absolute",
+                width: screenWidth,
+                height: screenWidth*.1*1.4,
+                bottom: Math.trunc(tabBarHeight*.5),
+                left: 0,
+                right: 0,
+                justifyContent: "center",
+                alignItems: "center",
+              }}>
+                <ButtonComponent />
+              </View>
+            );
+            ButtonComponentToShow = WrappedButtonComponent;
           }
-        };
-
-        const onLongPress = () => {
-          navigation.emit({
-            type: "tabLongPress",
-            target: route.key,
-          });
-        };
-
-        return (
-          <TouchableOpacity
-            key={route.key}
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={{ flex: 1 }}
-          >
-            <Text style={{ color: isFocused ? "#673ab7" : "#222" }}>
-              {label}
-            </Text>
-          </TouchableOpacity>
-        );
-      })
-      }
-    </View>
+          return (
+            <ButtonComponentToShow key={route.key} />
+          );
+        })}
+      </View>
+    </ZStack>
   );
 }
 
