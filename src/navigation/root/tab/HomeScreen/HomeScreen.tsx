cd68da18de218dbe45fc7dc48601afb1dba33130
Joshua Tag Howard <tag@jthoward.dev>
2023-10-23 21:34:09 -0400

    Add some placeholder home screen content

--- src/navigation/root/tab/HomeScreen/HomeScreen.tsx
+++ src/navigation/root/tab/HomeScreen/HomeScreen.tsx
@@ -1,15 +1,12 @@
 import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
 import { openURL } from "expo-linking";
-import { DateTime } from "luxon";
 import { Box, Button, HStack, Text, VStack } from "native-base";
-import { ImageBackground, ImageSourcePropType, PixelRatio, Share, StatusBar, TouchableOpacity, useWindowDimensions } from "react-native";
+import { PixelRatio, StatusBar, TouchableOpacity, useWindowDimensions } from "react-native";
 
-import CountdownView from "../../../../common/components/CountdownView";
 import { universalCatch } from "../../../../common/logging";
 import { useReactNavigationTheme } from "../../../../theme";
 
 import HeaderImage from "./HeaderImage";
-import SponsorCarousel from "./SponsorCarousel";
 
 /**
  * Component for home screen in main navigation
@@ -22,115 +19,129 @@ const HomeScreen = () => {
 
   return (
     <>
-      <StatusBar hidden = { false } />
+
+      <StatusBar hidden={false} />
       <VStack flexDirection="column" bgColor={bgColor} flex={1}>
         <Box
           flex={1}
           tintColor={bgColor}
           borderBottomColor="secondary.400"
           borderBottomWidth={2}
-          borderBottomStyle="solid">
+          borderBottomStyle="solid"
+        >
           <HeaderImage />
         </Box>
-        <ImageBackground
-          source={require("../../../../../assets/bg-geometric/blue.png") as ImageSourcePropType}
-          resizeMode="cover"
-          style={{ width: screenWidth, borderBottomColor: "#FFC72C", borderBottomWidth: 3, flex: 1 }}>
-          <Box height="23%">
-            {/* <PodcastPlayer />*/}
-            <Text
-              textAlign="center"
-              color="secondary.400"
-              fontFamily="headingBold"
-              fontSize={30/fontScale}
-              shadow="1"
-              style={{
-                textShadowColor: "secondary.300",
-                textShadowOffset: { width: 2, height: 1.5 },
-                textShadowRadius: 1
-              }}>{"Countdown 'til Marathon"}</Text>
-            <CountdownView endTime={DateTime.fromObject({ year: 2023, month: 3, day: 25, hour: 20 }).toMillis()} />
-          </Box>
-        </ImageBackground>
-        <Box flex={1}>
-          <SponsorCarousel />
+        {/* Placeholder "Welcome to DanceBlue 2024, we're getting things ready for you!\nPlease check back soon!" */}
+        <Box
+          flex={2}
+          justifyContent="center"
+          alignItems="center"
+          width={screenWidth}
+        >
+          <Text fontSize={fontScale * 30} marginX={"1/6"} textAlign="center">
+            Welcome to DanceBlue 2024, we're getting things ready for you!
+          </Text>
+          <Text fontSize={fontScale * 30} marginX={"1/6"} textAlign="center">
+            Please check back soon!
+          </Text>
         </Box>
         <Box flex={1} justifyContent="center">
           <HStack justifyContent="center">
             <Button
               onPress={() => {
-                openURL("https://danceblue.networkforgood.com").catch(universalCatch);
+                openURL("https://danceblue.networkforgood.com").catch(
+                  universalCatch
+                );
               }}
               width="2/5"
               backgroundColor="primary.600"
               _text={{ color: "secondary.400" }}
-              _pressed={{ opacity: 0.6 }}>
-            Donate #FTK!
+              _pressed={{ opacity: 0.6 }}
+            >
+              Donate #FTK!
             </Button>
             <Button
-              onPress={() => {
-                Share.share({ url: "https://danceblue.org/marathon-livestream" }).catch(universalCatch);
-              }}
-              width="2/5"
-              backgroundColor={"secondary.400"}
-              _text={{ color: "primary.600" }}
-              _pressed={{ opacity: 0.6 }}>
-              Share Zoom Link
-            </Button>
-            {/* <Button
               onPress={() => {
                 openURL("https://danceblue.org").catch(universalCatch);
               }}
               width="2/5"
               backgroundColor={"secondary.400"}
               _text={{ color: "primary.600" }}
-              _pressed={{ opacity: 0.6 }}>
+              _pressed={{ opacity: 0.6 }}
+            >
               Go to DanceBlue HQ
             </Button>
-            */}
           </HStack>
           <HStack justifyContent="center">
             <Button backgroundColor="transparent">
-              <TouchableOpacity onPress={() => {
-                openURL("https://www.facebook.com/danceblue/").catch(universalCatch);
-              }}>
+              <TouchableOpacity
+                onPress={() => {
+                  openURL("https://www.facebook.com/danceblue/").catch(
+                    universalCatch
+                  );
+                }}
+              >
                 <FontAwesome
                   name="facebook"
                   color="#0032A0"
-                  style={{ textAlignVertical: "center", fontSize: PixelRatio.get() * 8 }}
+                  style={{
+                    textAlignVertical: "center",
+                    fontSize: PixelRatio.get() * 8,
+                  }}
                 />
               </TouchableOpacity>
             </Button>
             <Button backgroundColor="transparent">
-              <TouchableOpacity onPress={() => {
-                openURL("https://www.instagram.com/uk_danceblue/").catch(universalCatch);
-              }}>
+              <TouchableOpacity
+                onPress={() => {
+                  openURL("https://www.instagram.com/uk_danceblue/").catch(
+                    universalCatch
+                  );
+                }}
+              >
                 <FontAwesome5
                   name="instagram"
                   color="#0032A0"
-                  style={{ textAlignVertical: "center", fontSize: PixelRatio.get() * 8 }}
+                  style={{
+                    textAlignVertical: "center",
+                    fontSize: PixelRatio.get() * 8,
+                  }}
                 />
               </TouchableOpacity>
             </Button>
             <Button backgroundColor="transparent">
-              <TouchableOpacity onPress={() => {
-                openURL("https://www.tiktok.com/@uk_danceblue").catch(universalCatch);
-              }}>
+              <TouchableOpacity
+                onPress={() => {
+                  openURL("https://www.tiktok.com/@uk_danceblue").catch(
+                    universalCatch
+                  );
+                }}
+              >
                 <FontAwesome5
                   name="tiktok"
                   color="#0032A0"
-                  style={{ textAlignVertical: "center", fontSize: PixelRatio.get() * 8 }}
+                  style={{
+                    textAlignVertical: "center",
+                    fontSize: PixelRatio.get() * 8,
+                  }}
                 />
               </TouchableOpacity>
             </Button>
             <Button backgroundColor="transparent">
-              <TouchableOpacity onPress={() => {
-                openURL("https://www.youtube.com/channel/UCcF8V41xkzYkZ0B1IOXntjg").catch(universalCatch);
-              }}>
+              <TouchableOpacity
+                onPress={() => {
+                  openURL(
+                    "https://www.youtube.com/channel/UCcF8V41xkzYkZ0B1IOXntjg"
+                  ).catch(universalCatch);
+                }}
+              >
                 <FontAwesome5
                   name="youtube"
                   color="#0032A0"
-                  style={{ textAlignVertical: "center", fontSize: PixelRatio.get() * 8 }}
+                  style={{
+                    textAlignVertical: "center",
+                    fontSize: PixelRatio.get() * 8,
+                  }}
                 />
               </TouchableOpacity>
             </Button>
